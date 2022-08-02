f68cb4d54a1460f99829275de52c2d053b6d210a
asaf <orsit70@gmail.com>
2022-08-02 21:21:53 +0300

    move to command added

--- Player.py
+++ Player.py
@@ -3,16 +3,16 @@ class S:  # short for status,stores stats
     currentHP: int
     posX: int
     posY: int
-    DamageTakenMultiplier:int
-    DamageDealtMultiplier:int
+    DamageTakenMultiplier: int
+    DamageDealtMultiplier: int
 
     def __init__(self, MaxHP, PosX, PosY):
         self.maxHP = MaxHP
-        self.currentHP=MaxHP
+        self.currentHP = MaxHP
         self.posX = PosX
         self.posY = PosY
-        self.DamageTakenMultiplier=1
-        self.DamageDealtMultiplier=1
+        self.DamageTakenMultiplier = 1
+        self.DamageDealtMultiplier = 1
 
 
 class player:
@@ -25,5 +25,9 @@ class player:
         self.s.posX = x
         self.s.posY = y
 
-    def PrintPos(self):
-        return "position:("+str(self.s.posX) + ", " + str(self.s.posY)+") \r HP: "+str(self.s.maxHP)+"/"+str(self.s.currentHP)
+    def TakeDamage(self,amount:int):
+        self.s.currentHP-=amount
+
+    def PrintStatus(self):
+        return "position:(" + str(self.s.posX) + ", " + str(self.s.posY) + ") \r HP: " + str(self.s.maxHP) + "/" + str(
+            self.s.currentHP)

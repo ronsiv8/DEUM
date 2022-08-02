e4b73072bc46c9d52561e66c7ed3761f346c2cb3
ronsiv8 <ronsivan81@gmail.com>
2022-08-02 18:52:48 +0300

    init

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import json, os
+
+from PIL import Image
+import json
 import discord
 import imageActions as IA
 
@@ -11,6 +14,9 @@ token = data['token']
 
 bot = discord.Bot()
 
+img = Image.open(directoryPath + "\\images\\bg.jpg")
+img = img.resize((3000, 3000))
+img.save(directoryPath + "\\images\\bg.jpg")
 
 @bot.event
 async def on_ready():
@@ -19,6 +25,10 @@ async def on_ready():
 
 @bot.slash_command(name='test', description='test', guild_ids=[756058242781806703])
 async def test(ctx):
-    await ctx.respond(IA.draw_grid_over_image(directoryPath + "\\images\\bg.jpg"))
+    await ctx.defer()
+    image = IA.draw_grid_over_image(directoryPath + "\\images\\bg.jpg")
+    image.savefig(directoryPath + "\\images\\grid.jpg")
+    img = discord.File(directoryPath + "\\images\\grid.jpg")
+    await ctx.respond(file=img)
 
 bot.run(token)

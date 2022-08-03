92f2048592490f6d5d9353e29e2739403b4682ca
asaf <orsit70@gmail.com>
2022-08-03 13:29:02 +0300

    added sobek abilities

--- Player.py
+++ Player.py
@@ -1,5 +1,8 @@
 import discord
-from Sobek import Sobek
+import os
+
+from PIL import Image
+from imageActions import crop_points
 
 
 class S:  # short for status,stores stats
@@ -18,7 +21,9 @@ class S:  # short for status,stores stats
         self.posY = PosY
         self.DamageTakenMultiplier = 1
         self.DamageDealtMultiplier = 1
-        self.abilityCooldowns=[]
+        self.bleedAmount = 0
+        self.bleedTimer = 0
+        self.abilityCooldowns = []
         for i in range(5):
             self.abilityCooldowns.append(0)
 
@@ -30,7 +35,7 @@ class player:
     def __init__(self, x, y, heroName):
         self.s = S(x, y)
         dict = {
-            "Sobek": Sobek,
+            "Sobek": hero.Sobek,
         }
         self.hero = dict[heroName](player=self)
 
@@ -39,8 +44,54 @@ class player:
         self.s.posY = y
 
     def TakeDamage(self, amount: int):
-        self.s.currentHP -= amount
+        self.s.currentHP -= amount * self.s.DamageTakenMultiplier
 
     def PrintStatus(self):
         return "position:(" + str(self.s.posX) + ", " + str(self.s.posY) + ") \r HP: " + str(self.s.maxHP) + "/" + str(
             self.s.currentHP)
+
+
+class hero:
+    heroName: str
+    heroObject = None
+
+    class Sobek:
+        myPlayer = None
+        image: Image
+        maxHP: int = 3000
+
+        def __init__(self, player):
+            self.myPlayer = player
+            self.image = Image.open(os.path.dirname(os.path.realpath(__file__)) + "\\images\\Sobek.png")
+            self.image = crop_points(self.image, [9, 165, 309, 465])
+            self.myPlayer.s.maxHP = self.maxHP
+            self.myPlayer.s.currentHP = self.myPlayer.s.maxHP
+
+        def a1(self, target: player):
+            target.TakeDamage(100)
+            target.s.bleedAmount += 100 * target.s.DamageTakenMultiplier * self.myPlayer.s.DamageDealtMultiplier
+            if target.s.bleedAmount > 0:
+                target.s.bleedAmount *= 2
+            target.s.bleedTimer = 2
+
+        def a2Possible(self, target: player):
+            for i in range(3):
+                for j in range(3):
+                    if not (i == 1 or j == 1):  # and grid[i,j]
+                        print("")
+
+        def a3(self, target: player):
+            dmgmult = 1
+            if target.s.bleedAmount > 0:
+                dmgmult = 2
+            target.TakeDamage(200 * dmgmult)
+            target.s.bleedAmount += 200 * target.s.DamageTakenMultiplier * dmgmult * self.myPlayer.s.DamageDealtMultiplier
+            target.s.bleedTimer = 2
+
+        def ult(self, target: player):
+            bonus = target.s.bleedAmount
+            target.TakeDamage(bonus * self.myPlayer.s.DamageDealtMultiplier)
+
+    def __init__(self, heroName: str):
+        self.heroName = heroName
+        self.heroObject = exec(str("Hero." + heroName))
